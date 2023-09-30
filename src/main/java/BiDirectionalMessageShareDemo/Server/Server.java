c62fd7cb2f432e1d460f1c321ea7de0c2d04fa42
PannaAryan <panna.codebase@gmail.com>
2023-09-30 22:32:29 +0600

    client to client chat

--- src/main/java/BiDirectionalMessageShareDemo/Server/Server.java
+++ src/main/java/BiDirectionalMessageShareDemo/Server/Server.java
@@ -7,21 +7,9 @@ import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.Executors;
 
-/**
- * A server program which accepts requests from clients to capitalize strings.
- * When a client connects, a new thread is started to handle it. Receiving
- * client data, capitalizing it, and sending the response back is all done on
- * the thread, allowing much greater throughput because more clients can be
- * handled concurrently.
- */
 public class Server {
 
-    /**
-     * Runs the server. When a client connects, the server spawns a new thread to do
-     * the servicing and immediately returns to listening. The application limits
-     * the number of threads via a thread pool (otherwise millions of clients could
-     * cause the server to run out of resources by allocating too many threads).
-     */
+
     public static void main(String[] args) throws Exception {
         try (var listener = new ServerSocket(59898)) {
             System.out.println("The capitalization server is running...");
